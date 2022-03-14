688c3af8342ad9f69625d8d8a4675244a663e67d
Ozan Uslan <ozanuslan@hotmail.com>
2022-03-14 21:02:53 +0300

    Lab1 - Fix comments

--- Lab1/lab1.ino
+++ Lab1/lab1.ino
@@ -7,8 +7,10 @@ const int pin2 = 4;
 const int pin3 = 5;
 const int pin4 = 6;
 
-int mapPin(int pin) {
-  switch(pin) {
+int mapPin(int pin)
+{
+  switch (pin)
+  {
   case 0:
     return pin0;
   case 1:
@@ -24,15 +26,18 @@ int mapPin(int pin) {
   return -1;
 }
 
-void turnOn(int pin) {
+void turnOn(int pin)
+{
   digitalWrite(mapPin(pin), HIGH);
 }
 
-void turnOff(int pin) {
+void turnOff(int pin)
+{
   digitalWrite(mapPin(pin), LOW);
 }
 
-void turnOnAll() {
+void turnOnAll()
+{
   turnOn(pin0);
   turnOn(pin1);
   turnOn(pin2);
@@ -40,95 +45,112 @@ void turnOnAll() {
   turnOn(pin4);
 }
 
-void blinkPin(int pin, int d) {
+void blinkPin(int pin, int d)
+{
   turnOn(pin);
   delay(d);
   turnOff(pin);
 }
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
-  while (!Serial) { // Serial portun hazır olmasını bekle.
+  while (!Serial)
+  {
     ;
   }
   Serial.println("---SETUP START---");
   Serial.println("Serial has been set up");
-  
+
   pinMode(pin0, OUTPUT);
   pinMode(pin1, OUTPUT);
   pinMode(pin2, OUTPUT);
   pinMode(pin3, OUTPUT);
   pinMode(pin4, OUTPUT);
   Serial.println("Pin[0-4] initialized");
-  
+
   Serial.println("---SETUP COMPLETE---\n");
 }
 
-int getCounterIncrement() {
+int getCounterIncrement()
+{
   int inc = 0;
 
-  do {
+  do
+  {
     Serial.print("Please enter increment value [1-31]: ");
-    while(!Serial.available()) {
+    while (!Serial.available())
+    {
     }
     inc = Serial.parseInt();
     Serial.println();
     Serial.print("You've entered: ");
     Serial.println(inc);
-    if(inc < 1 || inc > 31) {
+    if (inc < 1 || inc > 31)
+    {
       Serial.println("The value must be between 1 and 31");
     }
-  } 
-  while(inc < 1 || inc > 31);
+  } while (inc < 1 || inc > 31);
 
   return inc;
 }
 
-int getCounterDelay() {
+int getCounterDelay()
+{
   int d = 0;
 
-  do {
+  do
+  {
     Serial.print("Enter a delay(ms) for the counter [1-10000]: ");
-    while(!Serial.available()) {
+    while (!Serial.available())
+    {
     }
     d = Serial.parseInt();
     Serial.println();
     Serial.print("You've entered: ");
     Serial.println(d);
-    if(d < 1 || d > 10000) {
+    if (d < 1 || d > 10000)
+    {
       Serial.println("The delay must be between 1 and 10000");
     }
-  } 
-  while(d < 1 || d > 10000);
+  } while (d < 1 || d > 10000);
 
   return d;
 }
 
-void setPin(int pin, int on) {
-  if(on) {
+void setPin(int pin, int on)
+{
+  if (on)
+  {
     turnOn(pin);
-  } else {
+  }
+  else
+  {
     turnOff(pin);
   }
 }
 
-void displayIntOnPins(unsigned int num) {
+void displayIntOnPins(unsigned int num)
+{
   int binaryDigits[5];
-  for(int i = 0; i < 5; i++) {
+  for (int i = 0; i < 5; i++)
+  {
     binaryDigits[i] = num % 2;
     num /= 2;
   }
 
   Serial.print("Binary: ");
-  for(int i = 4; i >= 0; i--) {
+  for (int i = 4; i >= 0; i--)
+  {
     setPin(i, binaryDigits[i]);
     Serial.print(binaryDigits[i]);
   }
   Serial.println();
 }
 
-void count(unsigned int inc, int del) {
+void count(unsigned int inc, int del)
+{
   unsigned int count = 0;
   Serial.println("The counter is about to start, and count infinitely");
   Serial.println("If you would like to start a new counter please restart the Arduino");
@@ -138,7 +160,8 @@ void count(unsigned int inc, int del) {
   delay(1000);
   Serial.println("The counter starts in 1...");
   delay(1000);
-  while(true) {
+  while (true)
+  {
     Serial.print("Count:  ");
     Serial.println(count);
     displayIntOnPins(count);
@@ -148,7 +171,8 @@ void count(unsigned int inc, int del) {
   }
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   Serial.println("Starting a fresh counting sequence");
   unsigned int inc = getCounterIncrement();
