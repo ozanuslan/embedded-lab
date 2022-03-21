d2c3b81c1e25332d00b9bdaf43c7d81e2e9e0f31
Ozan Uslan <ozanuslan@hotmail.com>
2022-03-21 20:51:33 +0300

    Lab2 - Cleanup and comment

--- Lab2/lab2/lab2.ino
+++ Lab2/lab2/lab2.ino
@@ -1,14 +1,4 @@
 /*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
   The circuit:
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
@@ -22,48 +12,28 @@
  * 10K resistor:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
- */
+*/
 
 // include the library code:
 #include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-//LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-void waitInput()
-{
-  while (!Serial.available())
-    ;
-}
 
 const int big_buff_len = 101;
 
-int lcd_top = 0;
-int lcd_bot = 1;
-char text_top[big_buff_len] = "";
-char text_bot[big_buff_len] = "";
-char lcd_buf_top[17] = "";
-char lcd_buf_bot[17] = "";
-bool scroll_en_top = 0;
-bool scroll_en_bot = 0;
-bool scroll_dir_top = 0;
-bool scroll_dir_bot = 0;
-int scroll_ms_top = 1000;
-int scroll_ms_bot = 1000;
+int lcd_top = 0;                  // Top row LCD index
+int lcd_bot = 1;                  // Bottom row LCD index
+char text_top[big_buff_len] = ""; // User input string for top row
+char text_bot[big_buff_len] = ""; // User input string for bottom row
+char lcd_buf_top[17] = "";        // LCD buffer for top row
+char lcd_buf_bot[17] = "";        // LCD buffer for bottom row
+bool scroll_en_top = 0;           // Scroll enable for top row
+bool scroll_en_bot = 0;           // Scroll enable for bottom row
+bool scroll_dir_top = 0;          // Scroll direction for top row (0 for Left, 1 for Right)
+bool scroll_dir_bot = 0;          // Scroll direction for bottom row (0 for Left, 1 for Right)
+int scroll_ms_top = 1000;         // Scroll speed for top row in milliseconds
+int scroll_ms_bot = 1000;         // Scroll speed for bottom row in milliseconds
 
 void printMenu()
 {
@@ -89,6 +59,12 @@ void printMenu()
   Serial.println("\n9 - START LCD SEQUENCE");
 }
 
+void waitSerialInput()
+{
+  while (!Serial.available())
+    ;
+}
+
 int getChoice(int min, int max, char *msg, char *err)
 {
   int choice;
@@ -96,7 +72,7 @@ int getChoice(int min, int max, char *msg, char *err)
   do
   {
     Serial.print(msg);
-    waitInput();
+    waitSerialInput();
     choice = Serial.parseInt();
     Serial.println(choice);
     if (choice < min || choice > max)
@@ -136,7 +112,7 @@ void getText(char *text, int row)
     Serial.print("ENTER TEXT FOR ROW ");
     Serial.print(row);
     Serial.print(" (LENGTH [0-100]): ");
-    waitInput();
+    waitSerialInput();
     buf = Serial.readString();
     if (buf.length() > big_buff_len - 1)
     {
@@ -146,10 +122,13 @@ void getText(char *text, int row)
     }
   } while (buf.length() > big_buff_len - 1);
   strcpy(text, buf.c_str());
-  if(buf.length() < 16) {
-    for(int i = buf.length(); i < 16; i++) {
+  if (buf.length() < 16)
+  {
+    for (int i = buf.length(); i < 16; i++)
+    {
       text[i] = ' ';
     }
+    text[16] = '\0';
   }
   text[big_buff_len - 1] = '\0';
   Serial.println(text);
@@ -161,9 +140,18 @@ void printText(char *text, int row)
   lcd.print(text);
 }
 
-void writeToBuffer(char* text, char* buffer, int idx)
+void writeToBuffer(char *text, char *buffer, int idx)
 {
-  for(int i = 0; i < 16; i++) {
+  // Copies 16 character window from text to buffer starting at idx
+  // The copy wraps on the original text string
+  /**
+   * Example:
+   * text = "A text that is longer than 16 characters"
+   * idx = 26                          ^
+   * buffer = " 16 charactersA "
+   */
+  for (int i = 0; i < 16; i++)
+  {
     buffer[i] = text[(idx + i) % strlen(text)];
   }
   buffer[16] = '\0';
@@ -182,39 +170,54 @@ void startLCDSequence()
   writeToBuffer(text_bot, lcd_buf_bot, bot_idx);
   printText(lcd_buf_bot, lcd_bot);
 
-  while(1) {
-    if(scroll_en_top && time >= scroll_ms_top && time % scroll_ms_top == 0) {
-      if(scroll_dir_top) {
+  while (1)
+  {
+    if (scroll_en_top && time >= scroll_ms_top && time % scroll_ms_top == 0)
+    {
+      if (scroll_dir_top)
+      {
         top_idx--;
-      } else {
+      }
+      else
+      {
         top_idx++;
       }
-      if(top_idx < 0) {
+      if (top_idx < 0)
+      {
         top_idx = strlen(text_top) - 1;
-      } else if(top_idx > strlen(text_top) - 1) {
+      }
+      else if (top_idx > strlen(text_top) - 1)
+      {
         top_idx = 0;
       }
       writeToBuffer(text_top, lcd_buf_top, top_idx);
       printText(lcd_buf_top, lcd_top);
     }
-    if(scroll_en_bot && time >= scroll_ms_bot && time % scroll_ms_bot == 0) {
-      if(scroll_dir_bot) {
+    if (scroll_en_bot && time >= scroll_ms_bot && time % scroll_ms_bot == 0)
+    {
+      if (scroll_dir_bot)
+      {
         bot_idx--;
-      } else {
+      }
+      else
+      {
         bot_idx++;
       }
-      if(bot_idx < 0) {
+      if (bot_idx < 0)
+      {
         bot_idx = strlen(text_bot) - 1;
-      } else if(bot_idx > strlen(text_bot) - 1) {
+      }
+      else if (bot_idx > strlen(text_bot) - 1)
+      {
         bot_idx = 0;
       }
       writeToBuffer(text_bot, lcd_buf_bot, bot_idx);
       printText(lcd_buf_bot, lcd_bot);
     }
     time %= largest_delay;
-    
+
     delay(1);
-    time+=1;
+    time += 1;
   }
 }
 
@@ -273,14 +276,15 @@ void menu()
 
 void setup()
 {
+  // Initialize serial interface
   Serial.begin(9600);
   while (!Serial)
     ;
   Serial.println("\n---SETUP START---");
   Serial.println("Serial Initialized");
-  // set up the LCD's number of columns and rows:
+  // Set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  lcd.print("HELLO WORLD");
+  lcd.print("LCD INITIALIZED");
   Serial.println("LCD Initialized");
   Serial.println("---SETUP END---\n");
 }
