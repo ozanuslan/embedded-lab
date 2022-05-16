5e546dc03055627fc54143b2d4b44bdaf4baefa9
Ozan Uslan <ozanuslan@hotmail.com>
2022-05-16 20:38:38 +0300

    Lab9 - Small timing improvement

--- Lab9/src/main.cpp
+++ Lab9/src/main.cpp
@@ -5,7 +5,6 @@ const int LIGHT_SENSOR_PIN = A0;
 const int LED_ACTIVATION_VALUE = 500;
 const int SERIAL_OUTPUT_FREQUENCY = 500;
 unsigned long prevMillis;
-unsigned long currMillis;
 
 void setup()
 {
@@ -17,16 +16,15 @@ void setup()
 void loop()
 {
   int lightReading = analogRead(LIGHT_SENSOR_PIN);
-  currMillis = millis();
   bool ledOn = lightReading < LED_ACTIVATION_VALUE;
   digitalWrite(LED_PIN, ledOn ? HIGH : LOW);
-  if (currMillis - prevMillis >= SERIAL_OUTPUT_FREQUENCY)
+  if (millis() - prevMillis >= SERIAL_OUTPUT_FREQUENCY)
   {
     Serial.print("LIGHT SENSOR: ");
     Serial.println(lightReading);
     Serial.print("LED   STATUS: ");
     Serial.println(ledOn ? "ON" : "OFF");
     Serial.println();
-    prevMillis = currMillis;
+    prevMillis = millis();
   }
 }
