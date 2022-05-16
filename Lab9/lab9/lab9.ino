998873689be7dcc92c179a63d96859277674d9d9
Ozan Uslan <ozanuslan@hotmail.com>
2022-05-16 18:56:31 +0300

    Lab9 - Working IRL

---
+++ Lab9/lab9/lab9.ino
@@ -0,0 +1,30 @@
+const int LED_PIN = 3;
+const int LIGHT_SENSOR_PIN = A0;
+const int LED_ACTIVATION_VALUE = 500;
+const int SERIAL_OUTPUT_FREQUENCY = 500;
+unsigned long prevMillis;
+unsigned long currMillis;
+
+void setup()
+{
+    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(9600);
+    prevMillis = millis();
+}
+
+void loop()
+{
+    int lightReading = analogRead(LIGHT_SENSOR_PIN);
+    currMillis = millis();
+    bool ledOn = lightReading < LED_ACTIVATION_VALUE;
+    digitalWrite(LED_PIN, ledOn ? HIGH : LOW);
+    if (currMillis - prevMillis >= SERIAL_OUTPUT_FREQUENCY)
+    {
+        Serial.print("LIGHT SENSOR: ");
+        Serial.println(lightReading);
+        Serial.print("LED   STATUS: ");
+        Serial.println(ledOn ? "ON" : "OFF");
+        Serial.println();
+        prevMillis = currMillis;
+    }
+}
--- Lab9/src/main.cpp
+++ Lab9/src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const int LED_PIN = 3;
+const int LIGHT_SENSOR_PIN = A0;
+const int LED_ACTIVATION_VALUE = 500;
+const int SERIAL_OUTPUT_FREQUENCY = 500;
+unsigned long prevMillis;
+unsigned long currMillis;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  prevMillis = millis();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  int lightReading = analogRead(LIGHT_SENSOR_PIN);
+  currMillis = millis();
+  bool ledOn = lightReading < LED_ACTIVATION_VALUE;
+  digitalWrite(LED_PIN, ledOn ? HIGH : LOW);
+  if (currMillis - prevMillis >= SERIAL_OUTPUT_FREQUENCY)
+  {
+    Serial.print("LIGHT SENSOR: ");
+    Serial.println(lightReading);
+    Serial.print("LED   STATUS: ");
+    Serial.println(ledOn ? "ON" : "OFF");
+    Serial.println();
+    prevMillis = currMillis;
+  }
 }
