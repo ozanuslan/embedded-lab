e072ea747de5da75bd731244977058c0c3452727
Ozan Uslan <ozanuslan@hotmail.com>
2022-05-23 23:43:06 +0300

    Lab10 - Change sensor library, fix distance implementation

--- Lab10/src/main.cpp
+++ Lab10/src/main.cpp
@@ -9,16 +9,16 @@ const int TRIGGER_PIN = 6;
 const int MAX_DIGITS = 6; // Max number of digits to display
 
 LiquidCrystal_I2C lcd(0x3f, 16, 2);
-HCSR04 hc(TRIGGER_PIN, ECHO_PIN);
+UltraSonicDistanceSensor hc(TRIGGER_PIN, ECHO_PIN);
 
-int Buzzer_LED_Freq_Max = 1000; // ms
-int Buzzer_LED_Freq_Min = 10;   // ms
-int Dist_Max = 100;             // cm
-int Dist_Min = 10;              // cm
-int Current_Dist = 0;           // cm
-int Current_Freq = 0;           // ms
-int Update_Freq = 500;          // ms
-int Update_LCD = 200;           // ms
+const int Buzzer_LED_Freq_Max = 1000; // ms
+const int Buzzer_LED_Freq_Min = 10;   // ms
+const int Update_Freq = 500;          // ms
+const int Update_LCD = 200;           // ms
+const int Dist_Max = 100;             // cm
+const int Dist_Min = 10;              // cm
+int Current_Dist = 0;                 // cm
+int Current_Freq = 0;                 // ms
 
 unsigned long Prev_Millis = 0; // ms timestamp of last loop iteration
 
@@ -30,6 +30,8 @@ bool LED_Buz_On = false;
 
 void setup()
 {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
   Serial.begin(9600);
   lcd.begin();
   lcd.backlight();
@@ -37,7 +39,6 @@ void setup()
   lcd.print("Distance :");
   lcd.setCursor(0, 1);
   lcd.print("Frequency:");
-  pinMode(LED_PIN, OUTPUT);
   Prev_Millis = millis();
 }
 
@@ -82,56 +83,52 @@ float decideRegulatedVal(float cur_val, float max_val, float min_val)
 
 void loop()
 {
-  // Current_Dist = hc.dist();
-  // Current_Freq = calcFreq(hc.dist(), Dist_Min, Dist_Max, Buzzer_LED_Freq_Max, Buzzer_LED_Freq_Min);
-  // float display_dist = decideDisplayVal(Current_Dist, Dist_Max, Dist_Min);
-  // float display_freq = decideDisplayVal(Current_Freq, Buzzer_LED_Freq_Max, Buzzer_LED_Freq_Min);
-  // printLcd(display_dist, display_freq);
-
   // Update current millis
   unsigned long curr_millis = millis();
   unsigned long time_passed = curr_millis - Prev_Millis;
   Prev_Millis = curr_millis;
 
-  float raw_cur_dist = hc.dist();
+  float raw_cur_dist = hc.measureDistanceCm();
+  raw_cur_dist = raw_cur_dist < 0 ? Dist_Max : raw_cur_dist;
   float raw_cur_freq = calcFreq(raw_cur_dist, Dist_Min, Dist_Max, Buzzer_LED_Freq_Max, Buzzer_LED_Freq_Min);
   float cur_dist = decideRegulatedVal(raw_cur_dist, Dist_Max, Dist_Min);
   float cur_freq = decideRegulatedVal(raw_cur_freq, Buzzer_LED_Freq_Max, Buzzer_LED_Freq_Min);
 
-  if (LCD_Timer >= Update_LCD)
+  if (LCD_Timer >= Update_LCD) // update LCD
   {
     LCD_Timer %= Update_LCD;
     float display_dist = cur_dist;
     float display_freq = cur_freq;
     unsigned long before_lcd_print = millis();
     printLcd(display_dist, display_freq);
-    time_passed += millis() - before_lcd_print;
     curr_millis = millis();
+    time_passed += millis() - before_lcd_print;
   }
 
+  // advance timers
   LCD_Timer += time_passed;
   Freq_Timer += time_passed;
   LED_Buz_Timer += time_passed;
 
-  if (Freq_Timer >= Update_Freq)
+  if (Freq_Timer >= Update_Freq) // update distance and frequency values
   {
     Freq_Timer %= Update_Freq;
     Current_Dist = cur_dist;
     Current_Freq = cur_freq;
   }
 
-  if (LED_Buz_Timer >= Current_Freq)
+  if (Current_Dist <= Dist_Min) // too close
+  {
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else if (Current_Dist >= Dist_Max) // too far
+  {
+    digitalWrite(LED_PIN, LOW);
+  }
+  else if (LED_Buz_Timer >= Current_Freq) // if the timer has completed a cycle
   {
     LED_Buz_Timer %= Current_Freq;
-    if (LED_Buz_On)
-    {
-      digitalWrite(LED_PIN, LOW);
-      LED_Buz_On = false;
-    }
-    else
-    {
-      digitalWrite(LED_PIN, HIGH);
-      LED_Buz_On = true;
-    }
+    digitalWrite(LED_PIN, LED_Buz_On);
+    LED_Buz_On = !LED_Buz_On;
   }
 }
