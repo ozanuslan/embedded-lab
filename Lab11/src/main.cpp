4680f02911ac3d7eec430f8958bfd0f38d089ee8
Ozan Uslan <ozanuslan@hotmail.com>
2022-05-30 22:39:04 +0300

    Lab11 - DC not working, proper I/O added

--- Lab11/src/main.cpp
+++ Lab11/src/main.cpp
@@ -1,24 +1,26 @@
 #include <Arduino.h>
+#include <L293.h>
 
-#include <Servo.h>
+#define A 4   // A segment on the 7-segment display
+#define B 5   // B segment
+#define C 6   // C segment
+#define D 7   // D segment
+#define E 8   // E segment
+#define F 9   // F segment
+#define G 10  // G segment
+#define DP 11 // Dot on the corner of the LCD
 
-#define A 3   // A segment on the 7-segment display
-#define B 4   // B segment
-#define C 5   // C segment
-#define D 6   // D segment
-#define E 7   // E segment
-#define F 8   // F segment
-#define G 9   // G segment
-#define DP 10 // Dot on the corner of the LCD
+#define LEFT_BUTTON A1  // Left button input on A1 pin
+#define RIGHT_BUTTON A0 // Right button input on A0 pin
 
-#define SERVO_PIN 11
-#define LEFT_BUTTON 12
-#define RIGHT_BUTTON 13
+#define FWD_PIN 2
+#define BCK_PIN 3
+#define PWM_PIN 13
 
-bool segMode = 0;                   // 0 = Cathode, 1 = Anode
-int seg[]{A, B, C, D, E, F, G, DP}; // segment pins
-byte chars = 16;                    // max value in the array "Chars"
-byte Chars[16][9]{
+bool SEGMODE = 0;                    // 0 = Cathode, 1 = Anode
+int SEGS[]{A, B, C, D, E, F, G, DP}; // segment pins
+byte MAX_CHAR_VAL = 16;              // max value in the array "CHARS"
+byte CHARS[16][9]{
     // Corresponding low/high output for each hex number on the 7-segment display
     {'0', 1, 1, 1, 1, 1, 1, 0, 0}, // 0
     {'1', 0, 1, 1, 0, 0, 0, 0, 0}, // 1
@@ -38,62 +40,92 @@ byte Chars[16][9]{
     {'f', 1, 0, 0, 0, 1, 1, 1, 0}, // F/15
 };
 
-void setState(bool mode) // sets the whole segment state to "mode"
+void setSegmentState(bool mode) // sets the whole segment state to "mode"
 {
-  for (int i = 0; i <= 6; i++)
+  for (int i = 0; i < 8; i++)
   {
-    digitalWrite(seg[i], mode);
+    digitalWrite(SEGS[i], mode);
   }
 }
 
 void printSegment(int num) // print any number on the segment
 {
-  setState(segMode); // turn off the segment
+  setSegmentState(SEGMODE); // turn off the segment
+  if (num < 0)
+  {
+    digitalWrite(SEGS[7], HIGH); // turn on the dot
+  }
+  else
+  {
+    digitalWrite(SEGS[7], LOW); // turn off the dot
+  }
+  int n = num < 0 ? -num : num; // if num is negative, make it positive
   for (int i = 0; i < 7; i++)
   {
-    digitalWrite(seg[i], segMode == 0 ? Chars[num][i + 1] : !Chars[num][i + 1]);
+    digitalWrite(SEGS[i], SEGMODE == 0 ? CHARS[n][i + 1] : !CHARS[n][i + 1]);
   }
 }
 
-Servo myservo; // create servo object to control a servo
+bool LAST_RIGHT_READ = 1;
+bool LAST_LEFT_READ = 1;
+const int MAX_STEP = 4;
+int STEP = 0;
 
-int pos = 0; // variable to store the servo position
+// L293 motor(PWM_PIN, FWD_PIN, BCK_PIN);
 
 void setup()
 {
   Serial.begin(9600);
-  myservo.attach(SERVO_PIN);
 
   for (int i = 0; i < 8; i++)
   {
-    pinMode(seg[i], OUTPUT);
+    pinMode(SEGS[i], OUTPUT);
   }
-  for (int i = 0; i < 8; i++)
-  {
-    digitalWrite(seg[i], segMode);
-  }
+  setSegmentState(SEGMODE); // turn off the segment
 
   pinMode(LEFT_BUTTON, INPUT_PULLUP);
   pinMode(RIGHT_BUTTON, INPUT_PULLUP);
 
-  digitalWrite(DP, HIGH);
-}
+  pinMode(PWM_PIN, OUTPUT);
+  pinMode(FWD_PIN, OUTPUT);
+  pinMode(BCK_PIN, OUTPUT);
 
-bool last_right_read = 1;
-bool last_left_read = 1;
-byte count = 0;
+  digitalWrite(PWM_PIN, HIGH);
+  // analogWrite(FWD_PIN, 800);
+  digitalWrite(FWD_PIN, HIGH);
+  digitalWrite(BCK_PIN, LOW);
+
+  // analogWrite(BCK_PIN, 0);
+
+  printSegment(STEP);
+  // motor.forward(50);
+}
 
 void loop()
 {
-  bool right_read = digitalRead(RIGHT_BUTTON);
-  bool left_read = digitalRead(LEFT_BUTTON);
+  //----- UPDATE BUTTON STATES -----//
+  bool rightRead = digitalRead(RIGHT_BUTTON);
+  bool leftRead = digitalRead(LEFT_BUTTON);
+  bool isRightPressed = !LAST_RIGHT_READ && rightRead;
+  bool isLeftPressed = !LAST_LEFT_READ && leftRead;
+  LAST_RIGHT_READ = rightRead;
+  LAST_LEFT_READ = leftRead;
+  //-------------------------------//
 
-  if (right_read == 0 && last_right_read == 1)
+  //----- UPDATE & PRINT STEP -----//
+  if (!(isRightPressed && isLeftPressed)) // Both buttons pressed?
   {
-    count = (count + 1) % chars;
-    printSegment(count);
+    if (isRightPressed) // Right button to increase
+    {
+      STEP = STEP + 1 <= MAX_STEP ? STEP + 1 : STEP;
+    }
+    else if (isLeftPressed) // Left button to decrease
+    {
+      STEP = STEP - 1 >= -MAX_STEP ? STEP - 1 : STEP;
+    }
+    printSegment(STEP);
   }
-  last_right_read = right_read;
+  //-------------------------------//
 
-  delay(100);
+  delay(50);
 }
